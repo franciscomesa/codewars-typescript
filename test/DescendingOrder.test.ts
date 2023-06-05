6038893ab237e23007e368e0f7727efcb6bcd94a
Francisco Mesa <franciscomesa@gmail.com>
2023-06-05 11:26:23 +0100

    ✨ Add DescendingOrder function using one digit number

---
+++ test/DescendingOrder.test.ts
@@ -0,0 +1,19 @@
+function descendingOrder(number: number) {
+  return number
+}
+
+describe('Descending order training', () => {
+  // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order.
+  // Essentially, rearrange the digits to create the highest possible number.
+  // Input: 42145 Output: 54421
+  // Input: 145263 Output: 654321
+  // Input: 123456789 Output: 987654321
+  it('descendingOrder with one digit return the number without changes', () => {
+    const number = 9
+
+    const result = descendingOrder(number)
+
+    expect(result).toBe(number)
+  })
+
+})
