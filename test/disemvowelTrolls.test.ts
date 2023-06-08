577ec2f75207819249e7106c2e00e4dcbe4ca220
Francisco Mesa <franciscomesa@gmail.com>
2023-06-08 12:08:49 +0100

    :white_check_mark: Add return empty string if no text at entry

---
+++ test/disemvowelTrolls.test.ts
@@ -0,0 +1,21 @@
+export class Kata {
+  static disemvowel(str: string): string {
+    return str;
+  }
+}
+
+describe ('Disemvowel Trolls', () => {
+  /*
+  * Trolls are attacking your comment section!
+  * A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+  * Your task is to write a function that takes a string and return a new string with all vowels removed.
+  * For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+  * Note: for this kata y isn't considered a vowel.
+  * #STRINGS #REGULAREXPRESSIONS #FUNDAMENTALS
+  * */
+
+  it('return empty string if no text at entry', () => {
+    expect(Kata.disemvowel('')).toBe('')
+  })
+
+})
