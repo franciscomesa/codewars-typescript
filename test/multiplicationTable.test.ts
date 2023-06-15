c43741010454c028808f75a78144772584ff769d
Francisco Mesa <franciscomesa@gmail.com>
2023-06-15 11:10:11 +0100

    :test_tube: add return array with one element if size is 1

---
+++ test/multiplicationTable.test.ts
@@ -0,0 +1,31 @@
+function multiplicationTable (size: number): number[][] {
+  return []
+}
+describe('Multiplication Table', () => {
+  // https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/typescript
+  /*
+   * Your task, is to create N×N multiplication table, of size provided in parameter.
+   * For example, when given size is 3:
+   * 1 2 3
+   * 2 4 6
+   * 3 6 9
+   *
+   * For the given example, the return value should be:
+   *
+   * [[1,2,3],[2,4,6],[3,6,9]]
+   * #ARRAYS #FUNDAMENTALS
+   */
+
+  it('return empty array if size is 0', () => {
+    const result = multiplicationTable(0)
+
+    expect(result).toStrictEqual([])
+  })
+
+  it('return array with one element if size is 1', () => {
+    const result = multiplicationTable(1)
+
+    expect(result).toStrictEqual([[1]])
+  })
+
+})
