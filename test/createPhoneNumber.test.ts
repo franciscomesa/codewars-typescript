7b7ffa7965a7dad216fad05b521521795fcd1d6d
Francisco Mesa <franciscomesa@gmail.com>
2023-07-25 08:15:35 +0100

    :white_check_mark: Use regular expression

--- test/createPhoneNumber.test.ts
+++ test/createPhoneNumber.test.ts
@@ -1,6 +1,9 @@
 export function createPhoneNumber(numbers: number[]): string {
-  return ''
+  return numbers
+    .join('')
+    .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3')
 }
+
 describe('Create Phone Number should', () => {
   // https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/typescript
   /*
